e6ccdf3689b5609a0b4fdbd03fbe131457566788
Gidi Dafner <gidi@dreamvps.com>
2021-11-13 19:38:22 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
+
+import React, { useCallback, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const testAjax = useCallback(async () => {
+    const { data } = await axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/testAjax`,
+      {
+        hello: "world",
+      }
+    );
+
+    console.log(data);
+  }, []);
+
+  useEffect(() => {
+    testAjax();
+  }, [testAjax]);
+
+  return <div className={styles.wrapper}>APP</div>;
 }
 
 export default App;
